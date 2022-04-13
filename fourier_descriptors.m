e8b585d00a429110e33bcb022e01db129d0a018d
itellaetxe <inigotellaetxe@gmail.com>
2022-04-13 12:15:15 +0200

    Fourier Descriptors addition

---
+++ fourier_descriptors.m
@@ -0,0 +1,27 @@
+function [Z, ff] = fourier_descriptors(im)
+%FOURIER_DESCRIPTORS ==> Rangayyan's ff number.
+[~, xlocs, ylocs] = edge_features(im);
+N = length(xlocs);
+% Preallocate S for speed
+S = zeros(1, N);
+% Unefficient loop but simple
+for cK = 1:N
+    S(cK) = xlocs(cK) + 1j * ylocs(cK);
+end
+
+% Compute spectrum
+Z = fft(S, N);
+Z(1) = 0; % Remove centroid, not useful.
+% Normalize
+Zn = Z / abs(Z(2));
+
+% Get Rangayyan's Magic number "ff"
+s_1 = 0; s_2 = 0;
+for cI = 1:N/2
+    s_1 = abs(Zn(cI)) / cI + s_1; 
+    s_2 = abs(Zn(cI)) + s_2;
+end
+
+ff = 1 - (s_1(:) / s_2(:));
+
+end
