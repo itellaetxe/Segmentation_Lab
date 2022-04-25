d7c60a9b9eb6e69ca9961385da0c5964932b44ea
itellaetxe <inigotellaetxe@gmail.com>
2022-04-25 08:10:20 +0200

    Fourier Descriptors changes

--- fourier_descriptors.m
+++ fourier_descriptors.m
@@ -12,14 +12,15 @@ end
 % Compute spectrum
 Z = fft(S, N);
 Z(1) = 0; % Remove centroid, not useful.
+
 % Normalize
 Zn = Z / abs(Z(2) + Z(end));
 
 % Get Rangayyan's Magic number "ff"
 s_1 = 0; s_2 = 0;
 for cI = 1:N/2-1
-    s_1 = abs(Zn(cI+1)) / cI + s_1 +abs(Zn(end-cI+1)) / cI; 
-    s_2 = abs(Zn(cI+1)) + s_2 + +abs(Zn(end-cI+1));
+    s_1 = abs(Zn(cI+1)) / cI + abs(Zn(end-cI+1)) / cI + s_1; 
+    s_2 = abs(Zn(cI+1)) + abs(Zn(end-cI+1)) + s_2;
 end
 
 ff = 1 - (s_1/s_2);
