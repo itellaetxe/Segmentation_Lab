20ba6b4d0cf001bc6d481d85f87b13cbb4f68172
iisasa <92914512+iisasa@users.noreply.github.com>
2022-05-01 19:35:16 +0200

    Add files via upload

--- compute_compactness.m
+++ compute_compactness.m
@@ -1,4 +1,11 @@
 function Co = compute_compactness(bound, mask)
+    %{
+        Computes compactness.
+
+        IN: Boundary (perimeter) logical matrix, mask logical matrix
+        OUT: Compactness measure
+    %}
+
     P = sum(bound, 'all');
     A = sum(or(mask, bound), 'all');
 
