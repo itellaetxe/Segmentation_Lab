e83554e2a11758d15cc37fca9d801a1337404bbb
iisasa <92914512+iisasa@users.noreply.github.com>
2022-05-01 19:36:48 +0200

    Add files via upload

--- segment_element.m
+++ segment_element.m
@@ -1,14 +1,23 @@
 function [mask, bound] = segment_element(img)
+    %{
+        Computes mask and boundary of the image that is inserted.
+
+        IN: image matrix
+        OUT: logical mask matrix, logical boundary matrix
+    %}
 
     curr = imclose(img, ones(5));
-    thr = graythresh(curr);
 
+    % Otsu
+    thr = graythresh(curr);
     curr = curr > thr;
+
     curr = imclose(curr, ones(7));
 
     element = -ones(3);
     element(2,2) = 1;
     hits = bwhitmiss(curr, element);
+
     mask = xor(curr, hits);
     bound = bwmorph(mask, 'remove');
 end
