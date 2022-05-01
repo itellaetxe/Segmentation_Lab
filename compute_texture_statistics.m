95765232aa5b85344dcc43a8fafbbbb944e911ab
iisasa <92914512+iisasa@users.noreply.github.com>
2022-05-01 19:36:11 +0200

    Add files via upload

--- compute_texture_statistics.m
+++ compute_texture_statistics.m
@@ -1,10 +1,20 @@
 function [comat_props_x, comat_props_y] = compute_texture_statistics(img, mask)
+    %{
+        Computes texture statistical parameters using co-occurrence matrixes.
+
+        IN: Image matrix, mask logical matrix
+        OUT: Property struct with Energy, Correlation, Homogeneity and
+        Contrast parameters.
+    %}
+
+    img = imcomplement(img);
+    img = imgaussfilt(img, .5, 'FilterSize', [5, 5]);
     img = img.*mask;
 
     comat_x = graycomatrix(img, 'NumLevels', 8, 'Offset', [0, 1]);
-    comat_x(8, 8) = 0;
+    comat_x(1, 1) = 0;
     comat_y = graycomatrix(img, 'NumLevels', 8, 'Offset', [1, 0]);
-    comat_y(8, 8) = 0;
+    comat_y(1, 1) = 0;
 
     comat_props_x = graycoprops(comat_x, 'all');
     comat_props_y = graycoprops(comat_y, 'all');
